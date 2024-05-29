0acaa6dae6eaf25094869f69efe02aaa80f7b25c
jordan <jordan.stachura@gmail.com>
2024-05-29 01:44:35 -0400

    minor

--- archberry_pi/setup.pl
+++ archberry_pi/setup.pl
@@ -11,57 +11,68 @@
 # DO NOT USE THIS SCRIPT IF YOUR SD CARD IS NOT CALLED "mmcblk0"!!
 # IF I RAN THIS ON MY CHROMEBOOK IT WOULD WIPE THE FILESYSTEM!!
 # BE CAREFUL!! DOUBLE-CHECK WITH `lsblk` BEFORE RUNNING THIS SCRIPT!!
-#
-# Note: This creates temporary 'root' and 'boot' folders for mounting.
-# It will fail if directories with this name exist in the CWD.
 
 use strict; use warnings;
 
-# Check
 my $device = "/dev/mmcblk0";
+my $boot = $device."p1";
+my $root = $device."p2";
+my $mount = "/mnt/sd";
+my $temp = "/tmp/pi";
+my $image = "ArchLinuxARM-rpi-aarch64-latest.tar.gz";
+my $url = "http://os.archlinuxarm.org/os/$image";
+my $color = "\e[34m";
+my $reset = "\e[0m";
+
+# Setup
 unless (-b $device) { die "Device '$device' could not be found." }
 # TBD: Automated check for mmcblk0 safety?
+mkdir $temp or die "Temporary directory could not be created: $!";
+system("wget -P $temp $url") == 0 or die "Download failed: $!";
 
 # Partition
-print "\e[34m >>> Partitioning disk...\n\e[0m";
-my $commands = qq{o\nn\np\n1\n\n+512M\nn\np\n2\n\n\nw\n};
-open my $fdisk, "|-", "fdisk $device" or die "Unable to open fdisk: $!";
-print $fdisk $commands;
-close $fdisk or die "fdisk failed to close: $!";
-
-system "sync";
-sleep 2;
+print "$color>>> Partitioning disk...\n$reset";
+my $commands = <<'END';
+,256M,0c,
+,,,
+END
+system("sfdisk --quiet --wipe always $device $commands") == 0
+    or die "Partitioning failed: $!";
 
 # Format
-print "\e[34m >>> Formatting partitions...\n\e[0m";
-my $boot = $device."p1"; my $root = $device."p2";
-system("mkfs.vfat $boot") == 0 or die "mkfs failed to format $boot: $!";
-system("mkfs.ext4 $root") == 0 or die "mkfs failed to format $root: $!";
+print "$color>>> Formatting partitions...\n$reset";
+system("mkfs.vfat -F 32 $boot") == 0
+    or die "mkfs failed to format $boot: $!";
+my $options = "-E lazy_itable_init=0,lazy_journal_init=0";
+system("mkfs.ext4 $options -F $root") == 0
+    or die "mkfs failed to format $root: $!";
 
-mkdir "boot" or die "Error: Temporary 'boot' directory could not be created: $!";
-mkdir "root" or die "Error: Temporary 'root' directory could not be created: $!";
-system("mount $boot boot") == 0 or die "Failed to mount boot to $boot: $!";
-system("mount $root root") == 0 or die "Failed to mount root to $root: $!";
+mkdir $mount or die "Mount directory could not be created: $!";
+system("mount $root $mount") == 0 or die "Failed to mount: $!";
+mkdir "$mount/boot" or die "Boot directory could not be created: $!";
+system("mount $boot $root/boot") == 0 or die "Failed to mount: $!";
 
 # Install
-print "\e[34m >>> Installing Arch Linux ARM...\n\e[0mThis may take a few minutes...\n";
-my $image = "ArchLinuxARM-rpi-aarch64-latest.tar.gz";
-unless (-e $image) { 
-    system("wget http://os.archlinuxarm.org/os/$image") == 0
-        or die "Download failed: $!";
-}
-system("tar -xpf $image -C root") == 0 or die "Failed to extract image: $!";
+print "$color>>> Installing Arch Linux ARM...$reset\nThis may take a few minutes...\n";
+system("tar -xpf $temp/$image -C $mount") == 0
+    or die "Failed to extract image: $!";
 system("sync") == 0 or die "Sync failed: $!";
+
+
 system("mv ./root/boot/* boot") == 0 or die "Failed to move filesystem: $!";
 system("perl -i -pe 's/mmcblk0/mmcblk1/g' root/etc/fstab") == 0
     or die "Failed to edit fstab: $!";
 
+# Fix
+
+
 # Cleanup
-print "\e[34m >>> Cleaning up temporary directories...\n";
+print "$color>>> Cleaning up temporary directories...\n$reset";
 system("umount boot root") == 0 or die "Unmounting failed: $!";
 system("rm -rf ./root ./boot") == 0 or die "Directory cleanup failed: $!";
+# TODO: Clean up the Arch tarball too? Probably should?
 
-print "\e[34m >>> Success!\n\e[0m";
+print "$color>>> Success!\n$reset";
 
 print << 'FINISHED';
 Arch ARM installation finished. 
